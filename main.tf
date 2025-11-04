ac26098f97c4516bac497c7e18a96fba43ff830b
Thomas Thornton <thomas@thomasthornton.cloud>
2025-11-04 15:42:22 +0000

    Update address space for virtual network

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "azurerm_resource_group" "example" {
 # Virtual Network
 resource "azurerm_virtual_network" "example" {
   name                = "vnet-terraform-mcp"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/12"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
